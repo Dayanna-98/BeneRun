58ca504f0f075a0f9689342a036490048966ee94
smdn1 <sofian_madani@yahoo.com>
2026-02-03 17:45:37 +0100

    routes

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,6 @@ use App\Http\Controllers\TelephoneController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
