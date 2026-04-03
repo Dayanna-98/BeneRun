7700dbd91465a32b77995b180853a304c5ea626a
Sofian97M <sofian.madani@hes-so.ch>
2026-04-04 01:44:43 +0200

    Mise à jour de la BDD en mettant à jours les models et migrations.

--- app/Models/Mission.php
+++ app/Models/Mission.php
@@ -3,6 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Evenement; // ← ici
+use App\Models\User;
+use App\Models\MissionMedia;
+use App\Models\MissionContact;
+use App\Models\Competence;
 
 class Mission extends Model
 {
