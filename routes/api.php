3d1e768d300bb023f0353d626576e39f008470f6
Emma Rougeron <emmazeghdoud@gmail.com>
2026-04-27 19:43:23 +0200

    affectation mission

--- routes/api.php
+++ routes/api.php
@@ -43,6 +43,8 @@ Route::apiResource('/courses', CourseController::class);//->middleware('auth:san
 Route::apiResource('/documents', DocumentController::class);//->middleware('auth:sanctum');
 Route::apiResource('/evenements', EvenementController::class);//->middleware('auth:sanctum');
 Route::apiResource('/missions', MissionController::class);//->middleware('auth:sanctum');
+Route::patch('/missions/{id}/responsable', [MissionController::class, 'assignResponsable']);
+Route::post('/missions/{idMission}/inscriptions', [PostulationController::class, 'inscrireMission']);
 Route::apiResource('/postulations', PostulationController::class);//->middleware('auth:sanctum');
 Route::apiResource('/telephones', TelephoneController::class);//->middleware('auth:sanctum');
 
