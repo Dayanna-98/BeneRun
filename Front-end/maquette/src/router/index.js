bb014e1cbfda1a3bfb7a75046f11de96e0b78610
Emma Rougeron <emmazeghdoud@gmail.com>
2026-05-25 21:54:04 +0200

    changes

--- Front-end/maquette/src/router/index.js
+++ Front-end/maquette/src/router/index.js
@@ -72,7 +72,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/app/'),
   routes,
   scrollBehavior() {
     return { top: 0, left: 0, behavior: 'auto' }
