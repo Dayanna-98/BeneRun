26ea2ed9552a045a4e1ccb7cf8056410209d1466
dltp <dayanna.tenecela@hes-so.ch>
2026-05-28 21:35:49 +0200

    Fix Scramble timeout for API docs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        //Scramble
+        if (request()->is('docs/*')) {
+        set_time_limit(500);
+        }
+        
         Schema::defaultStringLength(191);
 
         VerifyEmail::toMailUsing(function ($notifiable, string $verificationUrl) {
